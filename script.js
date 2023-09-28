1d8862542ab6f4c92dfc927c0615f5479024d2c0
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-09-28 15:53:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+        var userAgent = navigator.userAgent;
+        var appName = navigator.appName;
+        var appVersion = navigator.appVersion;
+
+
+           function getBrowserInfo(userAgent) {
+            var browserInfo = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+            var browserName = browserInfo[1];
+            var browserVersion = browserInfo[2];
+
+            return {
+                name: browserName,
+                version: browserVersion
+            };
+        }
+
+   var browser = getBrowserInfo(userAgent);
+
+        // Display the browser information on the page
+        var browserInfoDiv = document.getElementById('browser-info');
+        browserInfoDiv.textContent = "You are using " + browser.name + " version " + browser.version;
+    
+
